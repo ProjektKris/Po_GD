e36ba0113829839105aae4ae0d68d81041689c60
ProjektKris <rbxkrisxyz@gmail.com>
2022-02-12 13:36:59 +0700

    use _physics_process for obstacle movement

--- src/obstacles/obstacle.gd
+++ src/obstacles/obstacle.gd
@@ -8,7 +8,7 @@ func _ready():
     if code != OK:
         push_error("Failed to connect _on_screen_exited: %d" % code) 
 
-func _process(delta: float):
+func _physics_process(delta: float):
     position -= Vector2(speed * delta, 0)
         
 func _on_screen_exited():
